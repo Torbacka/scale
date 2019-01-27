0b6c8415f933f130778e61afa3f870be37dd28e0
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-01-27 23:49:00 +0100

    improve the display function and added number array

--- scale.ino
+++ scale.ino
@@ -1,10 +1,23 @@
 
+#include <math.h>
+
 const int latchPin = 2;
 const int dataPin = 3;
 const int clockPin = 4;
 
 int currentLED = 0;
-
+byte numbers[10]={
+  0b00010100, //0
+  0b11110101, //1
+  0b10011000, //2
+  0b10110000, //3
+  0b01110001, //4
+  0b00110010, //5
+  0b00010011, //6 
+  0b11110100, //7
+  0b00010000, //8
+  0b01110000  //9
+};
 void setup() {
   Serial.begin(9600);
   pinMode(latchPin, OUTPUT);
@@ -20,25 +33,28 @@ void loop() {
       
       
       digitalWrite(latchPin, LOW);
-      if (i < 255) {
-        shiftOut(dataPin, clockPin, MSBFIRST, 0);
-      } else {
-        String info = String("i: ") + i + String("   i%255: ") + modff(23.42, 1.0);
-        Serial.println(info);
-        shiftOut(dataPin, clockPin, MSBFIRST, i/255);
-      }
-      shiftOut(dataPin, clockPin, MSBFIRST, i);
+      
+      shiftOut(dataPin, clockPin, MSBFIRST, 0b11110001);
+      
+      shiftOut(dataPin, clockPin, MSBFIRST, numbers[i % 10]);
         
       digitalWrite(latchPin, HIGH);
     
-      delay(10);
+      delay(1000);
     }
  
   
 }
 
-void display(float first) {
+void display(double first) {
+    double decimalPart;                                                              
+    int fractionPart = (int) ((modf(first, &decimalPart)*10)/10);  // first number after decimalPoint
 
+    int holeNumber = (int) decimalPart;
 
-  
+    int firstDigit, secondDigit, thirdDigit;
+    firstDigit = holeNumber % 10;
+    secondDigit = (holeNumber / 10) % 100;
+    thirdDigit = (holeNumber / 100) % 1000;
+    
 }
